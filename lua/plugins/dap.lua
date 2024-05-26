b590fcee0364c6abd722b33acb1c2a9678dee573
bruno.pina <brunomreis95@gmail.com>
2024-05-26 18:57:01 +0100

    chore: virtual text to dap

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -1,13 +1,13 @@
 return {
-  -- NOTE: Yes, you can install new plugins here!
   'mfussenegger/nvim-dap',
-  -- NOTE: And you can specify dependencies as well
   dependencies = {
     -- Creates a beautiful debugger UI
     'rcarriga/nvim-dap-ui',
 
     -- Required dependency for nvim-dap-ui
     'nvim-neotest/nvim-nio',
+
+    'theHamsta/nvim-dap-virtual-text',
 
     -- Installs the debug adapters for you
     'williamboman/mason.nvim',
