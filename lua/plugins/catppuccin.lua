c3a48049bd77c3547317446cf7ecea379fb99f64
bruno.pina <brunomreis95@gmail.com>
2024-05-27 18:01:05 +0100

    chore: changed themes

---
+++ lua/plugins/catppuccin.lua
@@ -0,0 +1,51 @@
+return {
+  {
+    'catppuccin/nvim',
+    name = 'catppuccin',
+    priority = 1000,
+    init = function()
+      vim.cmd.colorscheme 'catppuccin-macchiato'
+    end,
+    config = function()
+      local cat = require 'catppuccin'
+      cat.setup {
+        gitsigns = true,
+        treesitter = true,
+        treesitter_context = true,
+        mini = {
+          enabled = true,
+          -- indentscope_color = ""
+        },
+        neotree = true,
+        neotest = true,
+        dap = true,
+        dap_ui = true,
+        native_lsp = {
+          enabled = true,
+          virtual_text = {
+            errors = { 'italic' },
+            hints = { 'italic' },
+            warnings = { 'italic' },
+            information = { 'italic' },
+            ok = { 'italic' },
+          },
+          underlines = {
+            errors = { 'underline' },
+            hints = { 'underline' },
+            warnings = { 'underline' },
+            information = { 'underline' },
+            ok = { 'underline' },
+          },
+          inlay_hints = {
+            background = true,
+          },
+        },
+        telescope = {
+          enabled = true,
+          -- style = "nvchad"
+        },
+        which_key = false,
+      }
+    end,
+  },
+}
